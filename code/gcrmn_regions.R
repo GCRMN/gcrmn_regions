f551c510848697a1d14a44992fe7a356112d86eb
JWicquart <jeremywicquart@gmail.com>
2025-12-01 14:22:58 +0100

    split PERSGA 1 in two subregions

--- code/gcrmn_regions.R
+++ code/gcrmn_regions.R
@@ -11,83 +11,87 @@ data_gcrmn_regions <- st_read("data/meow/Marine_Ecoregions_Of_the_World__MEOW_.s
   st_transform(crs = 4326) %>% 
   # GCRMN region
   mutate(region = case_when(ECO_CODE_X %in% c(202, 203, 204, 205, 206, 207, 208, 209, 210, 211,
-                                                    120, 145, 144, 141, 140, 142, 143, 150, 151) ~ "Australia",
-                                  ECO_CODE_X %in% c(90, 91, 92) ~ "ROPME",
-                                  ECO_CODE_X %in% c(87, 88, 89) ~ "PERSGA",
-                                  ECO_CODE_X %in% c(103, 104, 105, 106, 107, 108) ~ "South Asia",
-                                  ECO_CODE_X %in% c(93, 94, 95, 96, 97, 98, 99, 100, 101, 102) ~ "WIO",
-                                  ECO_CODE_X %in% c(71, 72, 73, 74, 75, 76, 77) ~ "Brazil",
-                                  ECO_CODE_X %in% c(62, 63, 64, 65, 66, 67, 68, 69, 70, 43) ~ "Caribbean",
-                                  ECO_CODE_X %in% c(60, 61, 164, 165, 166, 167, 168, 
-                                                    169, 170, 171, 172, 173, 174) ~ "ETP",
-                                  ECO_CODE_X %in% c(109, 110, 111, 112, 113, 114, 115, 116, 117,
-                                                    118, 119, 121, 52, 51, 126, 127, 128, 129, 
-                                                    130, 139, 131, 132, 133) ~ "EAS",
-                                  ECO_CODE_X %in% c(122, 123, 124, 125, 134, 135, 136, 137, 138,
-                                                    149, 148, 147, 153, 154, 152, 159,
-                                                    162, 158, 161, 160, 156, 155, 157, 146) ~ "Pacific",
-                                  TRUE ~ NA_character_)) %>% 
+                                              120, 145, 144, 141, 140, 142, 143, 150, 151) ~ "Australia",
+                            ECO_CODE_X %in% c(90, 91, 92) ~ "ROPME",
+                            ECO_CODE_X %in% c(87, 88, 89) ~ "PERSGA",
+                            ECO_CODE_X %in% c(103, 104, 105, 106, 107, 108) ~ "South Asia",
+                            ECO_CODE_X %in% c(93, 94, 95, 96, 97, 98, 99, 100, 101, 102) ~ "WIO",
+                            ECO_CODE_X %in% c(71, 72, 73, 74, 75, 76, 77) ~ "Brazil",
+                            ECO_CODE_X %in% c(62, 63, 64, 65, 66, 67, 68, 69, 70, 43) ~ "Caribbean",
+                            ECO_CODE_X %in% c(60, 61, 164, 165, 166, 167, 168, 
+                                              169, 170, 171, 172, 173, 174) ~ "ETP",
+                            ECO_CODE_X %in% c(109, 110, 111, 112, 113, 114, 115, 116, 117,
+                                              118, 119, 121, 52, 51, 126, 127, 128, 129, 
+                                              130, 139, 131, 132, 133) ~ "EAS",
+                            ECO_CODE_X %in% c(122, 123, 124, 125, 134, 135, 136, 137, 138,
+                                              149, 148, 147, 153, 154, 152, 159,
+                                              162, 158, 161, 160, 156, 155, 157, 146) ~ "Pacific",
+                            TRUE ~ NA_character_)) %>% 
   # GCRMN subregion
-  mutate(subregion = case_when(# ROPME
-                                     ECO_CODE_X %in% c(90) ~ 1,
-                                     ECO_CODE_X %in% c(91) ~ 2,
-                                     ECO_CODE_X %in% c(92) ~ 3,
-                                     # PERSGA
-                                     ECO_CODE_X %in% c(87) ~ 1,
-                                     ECO_CODE_X %in% c(88) ~ 2,
-                                     ECO_CODE_X %in% c(89) ~ 3,
-                                     # WIO
-                                     ECO_CODE_X %in% c(93, 94, 95) ~ 1,
-                                     ECO_CODE_X %in% c(96) ~ 2,
-                                     ECO_CODE_X %in% c(97, 98) ~ 3,
-                                     ECO_CODE_X %in% c(99, 100) ~ 4,
-                                     ECO_CODE_X %in% c(101, 102) ~ 5,
-                                     # South Asia
-                                     ECO_CODE_X %in% c(106) ~ 1,
-                                     ECO_CODE_X %in% c(105) ~ 2,
-                                     ECO_CODE_X %in% c(103, 104) ~ 3,
-                                     ECO_CODE_X %in% c(107, 108) ~ 4,
-                                     # EAS
-                                     ECO_CODE_X %in% c(126, 127, 128) ~ 1,
-                                     ECO_CODE_X %in% c(129, 130, 131, 133, 138, 139) ~ 2,
-                                     ECO_CODE_X %in% c(115, 116, 117, 118) ~ 3,
-                                     ECO_CODE_X %in% c(119, 132) ~ 4,
-                                     ECO_CODE_X %in% c(109, 110, 111) ~ 5,
-                                     ECO_CODE_X %in% c(112, 113, 114) ~ 6,
-                                     ECO_CODE_X %in% c(51, 52, 121) ~ 7,
-                                     # Australia
-                                     ECO_CODE_X %in% c(142, 143, 202) ~ 1,
-                                     ECO_CODE_X %in% c(140, 141, 144, 145, 210, 211) ~ 2,
-                                     ECO_CODE_X %in% c(120) ~ 3,
-                                     ECO_CODE_X %in% c(151) ~ 4,
-                                     ECO_CODE_X %in% c(150) ~ 5,
-                                     # Pacific
-                                     ECO_CODE_X %in% c(121, 122, 123, 124, 125) ~ 1,
-                                     ECO_CODE_X %in% c(134, 135, 136, 137) ~ 2,
-                                     ECO_CODE_X %in% c(146, 147, 148, 149, 150) ~ 3,
-                                     ECO_CODE_X %in% c(152) ~ 4,
-                                     ECO_CODE_X %in% c(153, 154) ~ 5,
-                                     ECO_CODE_X %in% c(155, 156, 157) ~ 6,
-                                     ECO_CODE_X %in% c(158, 162, 159, 160, 161) ~ 7,
-                                     # ETP
-                                     ECO_CODE_X %in% c(60, 61) ~ 1,
-                                     ECO_CODE_X %in% c(166, 167, 168) ~ 2,
-                                     ECO_CODE_X %in% c(170, 171) ~ 3,
-                                     ECO_CODE_X %in% c(169, 172, 173, 174) ~ 4,
-                                     ECO_CODE_X %in% c(164, 165) ~ 5,
-                                     # Brazil
-                                     ECO_CODE_X %in% c(74) ~ 1,
-                                     ECO_CODE_X %in% c(75) ~ 2,
-                                     ECO_CODE_X %in% c(76) ~ 3,
-                                     ECO_CODE_X %in% c(77) ~ 4,
-                                     ECO_CODE_X %in% c(71, 72) ~ 5,
-                                     # Caribbean
-                                     ECO_CODE_X %in% c(62, 63) ~ 1,
-                                     ECO_CODE_X %in% c(64, 66) ~ 2,
-                                     ECO_CODE_X %in% c(65) ~ 3,
-                                     ECO_CODE_X %in% c(67, 68) ~ 4,
-                                     ECO_CODE_X %in% c(43, 69, 70) ~ 5,
-                                     TRUE ~ NA_integer_)) %>% 
+  mutate(subregion = case_when(
+    # ROPME
+    ECO_CODE_X %in% c(90) ~ 1,
+    ECO_CODE_X %in% c(91) ~ 2,
+    ECO_CODE_X %in% c(92) ~ 3,
+    # PERSGA
+    ECO_CODE_X %in% c(87) ~ 1,
+    ECO_CODE_X %in% c(88) ~ 3,
+    ECO_CODE_X %in% c(89) ~ 4,
+    # WIO
+    ECO_CODE_X %in% c(93, 94, 95) ~ 1,
+    ECO_CODE_X %in% c(96) ~ 2,
+    ECO_CODE_X %in% c(97, 98) ~ 3,
+    ECO_CODE_X %in% c(99, 100) ~ 4,
+    ECO_CODE_X %in% c(101, 102) ~ 5,
+    # South Asia
+    ECO_CODE_X %in% c(106) ~ 1,
+    ECO_CODE_X %in% c(105) ~ 2,
+    ECO_CODE_X %in% c(103, 104) ~ 3,
+    ECO_CODE_X %in% c(107, 108) ~ 4,
+    # EAS
+    ECO_CODE_X %in% c(126, 127, 128) ~ 1,
+    ECO_CODE_X %in% c(129, 130, 131, 133, 138, 139) ~ 2,
+    ECO_CODE_X %in% c(115, 116, 117, 118) ~ 3,
+    ECO_CODE_X %in% c(119, 132) ~ 4,
+    ECO_CODE_X %in% c(109, 110, 111) ~ 5,
+    ECO_CODE_X %in% c(112, 113, 114) ~ 6,
+    ECO_CODE_X %in% c(51, 52, 121) ~ 7,
+    # Australia
+    ECO_CODE_X %in% c(120) ~ 1,
+    ECO_CODE_X %in% c(145, 210, 211) ~ 2,
+    ECO_CODE_X %in% c(144) ~ 3,
+    ECO_CODE_X %in% c(140, 141) ~ 4,
+    ECO_CODE_X %in% c(142, 143, 202) ~ 5,
+    ECO_CODE_X %in% c(150) ~ 6,
+    ECO_CODE_X %in% c(151) ~ 7,
+    # Pacific
+    ECO_CODE_X %in% c(121, 122, 123, 124, 125) ~ 1,
+    ECO_CODE_X %in% c(134, 135, 136, 137) ~ 2,
+    ECO_CODE_X %in% c(146, 147, 148, 149, 150) ~ 3,
+    ECO_CODE_X %in% c(152) ~ 4,
+    ECO_CODE_X %in% c(153, 154) ~ 5,
+    ECO_CODE_X %in% c(155, 156, 157) ~ 6,
+    ECO_CODE_X %in% c(158, 162, 159, 160, 161) ~ 7,
+    # ETP
+    ECO_CODE_X %in% c(60, 61) ~ 1,
+    ECO_CODE_X %in% c(166, 167, 168) ~ 2,
+    ECO_CODE_X %in% c(170, 171) ~ 3,
+    ECO_CODE_X %in% c(169, 172, 173, 174) ~ 4,
+    ECO_CODE_X %in% c(164, 165) ~ 5,
+    # Brazil
+    ECO_CODE_X %in% c(74) ~ 1,
+    ECO_CODE_X %in% c(75) ~ 2,
+    ECO_CODE_X %in% c(76) ~ 3,
+    ECO_CODE_X %in% c(77) ~ 4,
+    ECO_CODE_X %in% c(71, 72) ~ 5,
+    # Caribbean
+    ECO_CODE_X %in% c(62, 63) ~ 1,
+    ECO_CODE_X %in% c(64) ~ 2,
+    ECO_CODE_X %in% c(66) ~ 3,
+    ECO_CODE_X %in% c(67, 68) ~ 4,
+    ECO_CODE_X %in% c(65) ~ 5,
+    ECO_CODE_X %in% c(43, 69, 70) ~ 6,
+    TRUE ~ NA_integer_)) %>% 
   st_as_sf() %>% 
   rename(ecoregion = ECOREGION) %>% 
   group_by(region, subregion, ecoregion) %>% 
@@ -101,6 +105,52 @@ data_gcrmn_regions <- st_read("data/meow/Marine_Ecoregions_Of_the_World__MEOW_.s
 data_gcrmn_regions <- nngeo::st_remove_holes(data_gcrmn_regions) %>% 
   st_transform(crs = 4326) %>% 
   st_make_valid()
+
+# 4. Split PERGSA 1 into PERSGA 1 and 2 ----
+
+## 4.1 Create PERSGA 2 polygon ----
+
+# (The polygon was manually created on Google Earth Engine)
+
+data_persga2 <- matrix(c(
+  31.379, 23.155,
+  32.544, 20.154,
+  34.126, 17.910,
+  42.981, 21.304,
+  42.711, 21.769,
+  40.844, 25.294,
+  37.822, 24.394,
+  35.724, 23.956,
+  35.619, 23.961,
+  35.490, 24.051,
+  31.379, 23.155), # closing (repeat the fisrt line)
+  ncol = 2, byrow = TRUE) %>% 
+  list(.) %>% 
+  st_polygon() %>% 
+  st_sfc(., crs = 4326) %>% 
+  st_as_sf()
+  
+## 4.2 
+
+data_persga1_before <- data_gcrmn_regions %>% 
+  filter(region == "PERSGA" & subregion == 1)
+
+persga2 <- st_intersection(data_persga1_before, data_persga2) %>% 
+  mutate(region = "PERSGA",
+         subregion = 2,
+         ecoregion = "Northern and Central Red Sea")
+
+persga1 <- st_difference(data_persga1_before, data_persga2) %>% 
+  mutate(region = "PERSGA",
+         subregion = 1,
+         ecoregion = "Northern and Central Red Sea")
+  
+data_gcrmn_regions <- data_gcrmn_regions %>% 
+  filter(!(region == "PERSGA" & subregion == 1)) %>% 
+  bind_rows(., persga2) %>% 
+  bind_rows(., persga1) 
+  
+rm(persga1, persga2, data_persga1_before, data_persga2)
 
 # 4. Export the data ----
 
@@ -186,6 +236,8 @@ ggplot() +
   geom_sf(data = data_gcrmn_regions, aes(fill = region)) +
   geom_sf(data = data_country, color = "#24252a", fill = "#dadfe1") +
   theme(text = element_text(family = "Open Sans"),
+        axis.ticks = element_blank(),
+        axis.text = element_blank(),
         legend.position = "bottom",
         legend.background = element_rect(fill = "transparent", color = NA),
         legend.title = element_blank(),
